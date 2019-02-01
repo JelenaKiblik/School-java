5bd4f662cc30eab26cd79b27858d42ba37b3837f
jelen <jekibl@ttu.ee>
2019-02-01 11:55:37 +0200

    check magic number's

--- EX01IdCode/src/ee/taltech/iti0202/idcode/IDCode.java
+++ EX01IdCode/src/ee/taltech/iti0202/idcode/IDCode.java
@@ -1,6 +1,12 @@
 package ee.taltech.iti0202.idcode;
 
 public class IDCode {
+    private static final int MIN_GENDER_NUMBER = 1;
+    private static final int MAX_GENDER_NUMBER = 6;
+    private static final int MIN_YEAR_NUMBER = 0;
+    private static final int MAX_YEAR_NUMBER = 99;
+    private static final int MIN_MONTH_NUMBER = 0;
+    private static final int MAX_MONTH_NUMBER = 12;
 
     private enum Gender {
         MALE, FEMALE
@@ -11,19 +17,19 @@ public class IDCode {
     }
 
     private static boolean isGenderNumberCorrect(String idCode) {
-        return idCode.charAt(0) >= 1 || idCode.charAt(0) <= 6;
+        return idCode.charAt(0) >= MIN_GENDER_NUMBER || idCode.charAt(0) <= MAX_GENDER_NUMBER;
     }
 
     private static boolean isYearNumberCorrect(String idCode) {
-        return Integer.parseInt(idCode.substring(1,3)) >= 0 || Integer.parseInt(idCode.substring(1,3)) <= 99;
+        return Integer.parseInt(idCode.substring(1, 3)) >= MIN_YEAR_NUMBER || Integer.parseInt(idCode.substring(1, 3)) <= MAX_YEAR_NUMBER;
     }
 
     private static boolean isMonthNumberCorrect(String idCode) {
-        return Integer.parseInt(idCode.substring(3,5)) > 0 || Integer.parseInt(idCode.substring(3,5)) <= 12;
+        return Integer.parseInt(idCode.substring(3, 5)) > MIN_MONTH_NUMBER || Integer.parseInt(idCode.substring(3, 5)) <= MAX_MONTH_NUMBER;
     }
 
     private static boolean isDayNumberCorrect(String idCode) {
-        return Integer.parseInt(idCode.substring(5,7)) > 0 || Integer.parseInt(idCode.substring(5,7)) <= 31;
+        return Integer.parseInt(idCode.substring(5, 7)) > 0 || Integer.parseInt(idCode.substring(5, 7)) <= 31;
     }
 
     private static boolean isQueueNumberCorrect(String idCode) {
