b15f3713365f4391c5e4bac84804d6b95cf66678
jelen <jekibl@ttu.ee>
2019-02-01 10:38:35 +0200

    first commit

---
+++ EX01IdCode/src/ee/taltech/iti0202/idcode/IDCode.java
@@ -0,0 +1,52 @@
+package ee.taltech.iti0202.idcode;
+
+public class IDCode {
+
+    private enum Gender {
+        MALE, FEMALE
+    }
+
+    public static boolean isIDCodeCorrect(String idCode) {
+        return false;
+    }
+
+    private static boolean isGenderNumberCorrect(String idCode) {
+        return false;
+    }
+
+    private static boolean isYearNumberCorrect(String idCode) {
+        return false;
+    }
+
+    private static boolean isMonthNumberCorrect(String idCode) {
+        return false;
+    }
+
+    private static boolean isDayNumberCorrect(String idCode) {
+        return false;
+    }
+
+    private static boolean isQueueNumberCorrect(String idCode) {
+        return false;
+    }
+
+    private static boolean isControlNumberCorrect(String idCode) {
+        return false;
+    }
+
+    private static boolean isLeapYear(int fullYear) {
+        return false;
+    }
+
+    public static String getInformationFromIDCode(String idCode) {
+        return "";
+    }
+
+    public static Gender getGender(String idCode) {
+        return null;
+    }
+
+    public static int getFullYear(String idCode) {
+        return 0;
+    }
+}
