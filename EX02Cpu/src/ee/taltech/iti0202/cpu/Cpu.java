b55251f01af27a7325d8af675483daff3f28b397
jelen <jekibl@ttu.ee>
2019-02-10 18:37:23 +0200

    check code

--- EX02Cpu/src/ee/taltech/iti0202/cpu/Cpu.java
+++ EX02Cpu/src/ee/taltech/iti0202/cpu/Cpu.java
@@ -5,13 +5,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Cpu {
-
-    private static final int VALUE_TO_BE_MODIFIED = 0;
     private static final int ARITHMETIC_FUNCTION = 1;
     private static final int ARITHMETIC_VALUE = 2;
     private static final int COMPARATOR_KEY = 4;
     private static final int COMPARATOR_FUNCTION = 5;
     private static final int COMPARATOR_VALUE = 6;
+    private static final int LINE_LENGTH = 7;
     private static boolean condition;
 
     public static Map<String, Integer> compute(String instructions) {
@@ -20,12 +19,14 @@ public class Cpu {
 
         for (String s : registers) {
             String[] steps = s.split("\\s");
-            String valueToBeModified = steps[0];
-            int arithmeticFunction = (steps[1].equals("inc") ? 1 : -1);
-            int arithmeticValue = Integer.parseInt(steps[2]);
-            String comparatorKey = steps[4];
-            String comparatorFunction = steps[5];
-            int comparatorValue = Integer.parseInt(steps[6]);
+
+        for (int i = 0; i < steps.length; i += LINE_LENGTH) {
+            String valueToBeModified = steps[i];
+            int arithmeticFunction = (steps[i + ARITHMETIC_FUNCTION].equals("inc") ? 1 : -1);
+            int arithmeticValue = Integer.parseInt(steps[i + ARITHMETIC_VALUE]);
+            String comparatorKey = steps[i + COMPARATOR_KEY];
+            String comparatorFunction = steps[i + COMPARATOR_FUNCTION];
+            int comparatorValue = Integer.parseInt(steps[i + COMPARATOR_VALUE]);
 
             if (!map.containsKey(comparatorKey)) {
                 map.put(comparatorKey, 0);
@@ -38,70 +39,69 @@ public class Cpu {
                 case "==":
                     if (map.get(comparatorKey) == comparatorValue) {
                         condition = true;
+                        System.out.println(comparatorKey + " == " + comparatorValue);
                     }
                     break;
                 case "!=":
                     if (map.get(comparatorKey) != comparatorValue) {
                         condition = true;
+                        System.out.println(comparatorKey + " != " + comparatorValue);
                     }
                     break;
                 case "<":
                     if (map.get(comparatorKey) < comparatorValue) {
                         condition = true;
+                        System.out.println(comparatorKey + " < " + comparatorValue);
                     }
                     break;
                 case "<=":
                     if (map.get(comparatorKey) <= comparatorValue) {
                         condition = true;
+                        System.out.println(comparatorKey + " <= " + comparatorValue);
                     }
                     break;
                 case ">":
                     if (map.get(comparatorKey) > comparatorValue) {
                         condition = true;
+                        System.out.println(comparatorKey + " > " + comparatorValue);
                     }
                     break;
                 case ">=":
                     if (map.get(comparatorKey) >= comparatorValue) {
                         condition = true;
+                        System.out.println(comparatorKey + " >= " + comparatorValue);
                     }
                     break;
-                    default:
-                        throw new RuntimeException("Unexpected operator");
-            }
+                default:
+                    throw new RuntimeException("Unexpected operator");
+                }
 
             if (condition) {
                 map.replace(valueToBeModified, map.get(valueToBeModified) + (arithmeticFunction * arithmeticValue));
             }
 
-            System.out.println(valueToBeModified);
-            System.out.println(arithmeticFunction);
-            System.out.println(arithmeticValue);
-            System.out.println(comparatorKey);
-            System.out.println(comparatorFunction);
-            System.out.println(comparatorValue);
-            System.out.println(Arrays.toString(steps));
+            }
         }
         return map;
     }
 
-
     public static void main(String[] args) {
         Map<String, Integer> res = compute(
-                "b inc 5 if a > 1\n"
-                        + "a inc 1 if b < 5\n"
-                        + "c dec -10 if a >= 1\n"
-                        + "c inc -20 if c == 10"
+        "b inc 5 if a > 1\n"
+                + "a inc 1 if b < 5\n"
+                + "c dec -10 if a >= 1\n"
+                + "c inc -20 if c == 10"
         );
         System.out.println(res); // {a=1, b=0, c=-10}
 
-//        res = compute(
-//                "b inc 7 if a > 4\n" +
-//                        "a inc 1 if c < 13\n" +
-//                        "c dec -10 if a >= 1\n" +
-//                        "c inc -20 if c == 10\n" +
-//                        "abc inc 100 if a != -23\n" +
-//                        "a inc 2 if a <= 0"
-//        );
-//        System.out.println(res); // {a=1, b=0, c=-10, abc=100}
+        res = compute(
+                "b inc 7 if a > 4\n" +
+                        "a inc 1 if c < 13\n" +
+                        "c dec -10 if a >= 1\n" +
+                        "c inc -20 if c == 10\n" +
+                        "abc inc 100 if a != -23\n" +
+                        "a inc 2 if a <= 0"
+        );
+        System.out.println(res); // {a=1, b=0, c=-10, abc=100}
     }
 }
