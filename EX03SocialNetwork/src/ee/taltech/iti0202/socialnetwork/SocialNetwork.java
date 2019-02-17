27596e75b73fe69cc94dbd4a2d1d6f13f2b05c94
jelen <jekibl@ttu.ee>
2019-02-17 16:52:14 +0200

    check feed

--- EX03SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
+++ EX03SocialNetwork/src/ee/taltech/iti0202/socialnetwork/SocialNetwork.java
@@ -1,12 +1,17 @@
 package ee.taltech.iti0202.socialnetwork;
 import ee.taltech.iti0202.socialnetwork.feed.Feed;
 import ee.taltech.iti0202.socialnetwork.group.Group;
+import ee.taltech.iti0202.socialnetwork.message.Message;
 import ee.taltech.iti0202.socialnetwork.user.User;
+
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class SocialNetwork {
     private Set<Group> socialNetworkList = new HashSet<>();
+    private List<Message> userMessages = new ArrayList<>();
 
     public void registerGroup(Group group) {
         socialNetworkList.add(group);
@@ -18,7 +23,7 @@ public class SocialNetwork {
 
     public Feed getFeedForUser(User user) {
         if (user != null) {
-            return new Feed(null, null);
+            return new Feed(user, userMessages);
         } else {
             return null;
         }
