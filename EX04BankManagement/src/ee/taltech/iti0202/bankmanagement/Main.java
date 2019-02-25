37b1b6c44f1f1fb901fd0997426c2f8e0994ea07
jelen <jekibl@ttu.ee>
2019-02-25 19:37:02 +0200

    check style

--- EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/Main.java
+++ EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/Main.java
@@ -8,24 +8,24 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        Bank SEB = new Bank("SEB");
-        Bank LHV = new Bank("LHV");
-        Bank swedbank = new Bank("Swedbank");
-
-        Person peep = new Person("Peep", "Tamm", 30, Person.Gender.MALE, 1600);
-        Person anne = new Person("Anne", "Kask", 27, Person.Gender.FEMALE, 2000);
-        Person valdo = new Person("Valdo", "Sepp", 56, Person.Gender.MALE, 1300);
-        Person malle = new Person("Malle", "Kuusk", 69, Person.Gender.FEMALE, 600);
-        Person juulius = new Person("Juulius", "Tipikas", 19, Person.Gender.MALE, 400);
-
+//        Bank SEB = new Bank("SEB");
+//        Bank LHV = new Bank("LHV");
+//        Bank swedbank = new Bank("Swedbank");
+//
+//        Person peep = new Person("Peep", "Tamm", 30, Person.Gender.MALE, 1600);
+//        Person anne = new Person("Anne", "Kask", 27, Person.Gender.FEMALE, 2000);
+//        Person valdo = new Person("Valdo", "Sepp", 56, Person.Gender.MALE, 1300);
+//        Person malle = new Person("Malle", "Kuusk", 69, Person.Gender.FEMALE, 600);
+//        Person juulius = new Person("Juulius", "Tipikas", 19, Person.Gender.MALE, 400);
+//
         System.out.println("- - - - DEBIT - - - -");
-
-
-        System.out.println(peep.getBankCard().isEmpty()); // true
-
-        BankCard peepDebitCard = BankCard.createCard(BankCard.CardType.DEBIT, SEB, peep);
-
-        System.out.println(peep.getBankCard().isEmpty()); // false
+//
+//
+//        System.out.println(peep.getBankCard().isEmpty()); // true
+//
+//        BankCard peepDebitCard = BankCard.createCard(BankCard.CardType.DEBIT, SEB, peep);
+//
+//        System.out.println(peep.getBankCard().isEmpty()); // false
 //        System.out.println(peepDebitCard == peep.getBankCard().get()); // true
 //        System.out.println(SEB.getCustomers().contains(peep)); // true
 //
