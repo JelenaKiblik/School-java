ecdd5b71d10bb5cf532e861bd7e5066f8ffc5fa6
jelen <jekibl@ttu.ee>
2019-03-01 11:11:56 +0200

    check

--- EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/card/BankCard.java
+++ EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/card/BankCard.java
@@ -57,7 +57,10 @@ public abstract class BankCard {
      * @throws TransactionException Thrown if given value is zero or less.
      */
     public void deposit(BigDecimal value) throws TransactionException {
-        if (value.compareTo(BigDecimal.valueOf(0)) > 0) balance = balance.add(value);
+        BigDecimal zero = new BigDecimal("0");
+        if (value.compareTo(zero) > -1) {
+            balance = balance.add(value);
+        }
     }
 
     /**
