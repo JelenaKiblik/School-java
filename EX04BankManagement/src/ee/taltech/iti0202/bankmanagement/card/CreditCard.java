f20a22f4203e595fb93489cd78fb2ba61c25a8ef
jelen <jekibl@ttu.ee>
2019-03-05 20:07:15 +0200

    check style

--- EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/card/CreditCard.java
+++ EX04BankManagement/src/ee/taltech/iti0202/bankmanagement/card/CreditCard.java
@@ -4,23 +4,22 @@ import java.math.BigDecimal;
 
 public final class CreditCard extends BankCard {
 
-    final BigDecimal startingBalance = BigDecimal.valueOf(10000);
-    final BigDecimal allowedDebt = BigDecimal.valueOf(5000);
-    BigDecimal balance;
+    private final BigDecimal startingMoney = BigDecimal.valueOf(10000);
+    private final BigDecimal debtLimit = BigDecimal.valueOf(5000);
 
     CreditCard() {
-        balance = new BigDecimal("10000");
+        super.balance = startingMoney;
     }
 
     @Override
     public BigDecimal withdraw(BigDecimal value) throws TransactionException {
-//        BigDecimal zero = new BigDecimal("0");
-//        if (balance.subtract(value).compareTo(zero) < -5000 || value.compareTo(zero) < 1) {
-//            throw new TransactionException("Error!!!");
-//        } else {
-//            balance = balance.subtract(value);
-//        }
-//        return zero;
+        if (value.compareTo(BigDecimal.valueOf(0)) <= 0) {
+            throw new TransactionException("Error!!");
+        }
+
+        if (value.compareTo(balance.add(debtLimit)) <= 0) {
+            balance = balance.subtract(value);
+        }
         return value;
     }
 
@@ -39,4 +38,5 @@ public final class CreditCard extends BankCard {
         }
         return BigDecimal.valueOf(0);
     }
+
 }
