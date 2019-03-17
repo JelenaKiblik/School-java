d2f835b89a2302fae6b2c9d9a3372eefc6a87d37
jelen <jekibl@ttu.ee>
2019-03-17 14:12:25 +0200

    check style

--- EX06Files/src/ee/taltech/iti0202/files/exception/FileReaderException.java
+++ EX06Files/src/ee/taltech/iti0202/files/exception/FileReaderException.java
@@ -1,5 +1,21 @@
 package ee.taltech.iti0202.files.exception;
 
-public class FileReaderException {
+public class FileReaderException extends  RuntimeException {
+    public FileReaderException() {
+        super();
+    }
+
+    public FileReaderException(String message) {
+        super(message);
+    }
+
+    public FileReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileReaderException(Throwable cause) {
+        super(cause);
+    }
+
 }
 
