90ca589aa8f620d05c1ae90c3c537e0ce18ec3f8
jelen <jekibl@ttu.ee>
2019-03-17 19:04:06 +0200

    check style

--- EX06Files/src/ee/taltech/iti0202/files/input/InputFilesScanner.java
+++ EX06Files/src/ee/taltech/iti0202/files/input/InputFilesScanner.java
@@ -1,8 +1,6 @@
 package ee.taltech.iti0202.files.input;
 import ee.taltech.iti0202.files.exception.FileReaderException;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,15 +10,15 @@ public class InputFilesScanner implements InputFilesReader {
     @Override
     public List<String> readTextFromFile(String filename) {
         List<String> text = new ArrayList<>();
-        File file =new File(filename);
-        Scanner sc = null;
+        File file = new File(filename);
+        Scanner scanner = null;
         try {
-            sc = new Scanner(file);
+            scanner = new Scanner(file);
         } catch (Exception e) {
             throw new FileReaderException("No such file", e.getCause());
         }
-        while(sc.hasNextLine()){
-            text.add(sc.nextLine());
+        while (scanner.hasNextLine()) {
+            text.add(scanner.nextLine());
         }
         return text;
     }
