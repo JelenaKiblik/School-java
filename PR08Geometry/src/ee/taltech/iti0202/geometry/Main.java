f05fb323118fe72198fabb94b880bdfff6a31646
jelen <jekibl@ttu.ee>
2019-03-26 17:18:05 +0200

    check style

--- PR08Geometry/src/ee/taltech/iti0202/geometry/Main.java
+++ PR08Geometry/src/ee/taltech/iti0202/geometry/Main.java
@@ -5,7 +5,8 @@ import ee.taltech.iti0202.geometry.shape.Triangle;
 public class Main {
 
     public static void main(String[] args) {
-        Triangle tr = new Triangle("k", 3, Triangle.Color.Blue, 40);
-        System.out.println(tr);
+        Canvas canvas = new Canvas();
+        Triangle triangle = new Triangle("kolmnurk", 3, Triangle.Color.Blue, 40);
+        System.out.println(triangle);
     }
 }
