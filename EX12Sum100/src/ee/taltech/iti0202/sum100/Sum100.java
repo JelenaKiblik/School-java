0319963c6bd8cd42a36e7412029f1fdf514aff81
jelen <jekibl@ttu.ee>
2019-04-23 15:46:03 +0300

    check style

---
+++ EX12Sum100/src/ee/taltech/iti0202/sum100/Sum100.java
@@ -0,0 +1,45 @@
+package ee.taltech.iti0202.sum100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum100 {
+
+    public static List<String> calcSums() {
+        List<String> result = new ArrayList<>();
+        helpFunction("123456789", new StringBuilder(),0, 0,  result);
+        return result;
+    }
+
+    static void helpFunction(String str, StringBuilder builder, int sumInProcess, int sum, List<String> result) {
+        if (sumInProcess == str.length()) {
+            if (sum == 100)
+                result.add(builder.toString());
+            return;
+        }
+
+        int length = builder.length();
+
+        for (int i = sumInProcess; i < str.length(); i++) {
+            int number = Integer.parseInt(str.substring(sumInProcess, i + 1));
+
+            if (sumInProcess == 0) {
+                builder.append(number);
+                helpFunction(str, builder,i + 1, sum + number, result);
+
+            } else {
+                builder.append("+").append(number);
+                helpFunction(str, builder,i + 1, sum + number, result);
+                builder.setLength(length);
+
+                builder.append("-").append(number);
+                helpFunction(str, builder,i + 1, sum - number, result);
+            }
+            builder.setLength(length);
+        }
+    }
+
+    public static void main(String[] args) {
+        calcSums();
+    }
+}
