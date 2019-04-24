3b549ac5840bc369300a80292a49718a17d3bfad
jelen <jekibl@ttu.ee>
2019-04-24 09:48:17 +0300

    check style

--- KT2/src/ee/taltech/iti0202/kt2/Exam.java
+++ KT2/src/ee/taltech/iti0202/kt2/Exam.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Exam {
 
-    public static final int NUMBER = 6;
     /**
      * Given lists nums1 and nums2 of the same length,
      * for every element in nums1, consider the corresponding
@@ -17,28 +16,14 @@ public class Exam {
      * matchUp([1, 2, 3], [2, 3, 3]) => 2
      */
     public static int matchUp(List<Integer> a, List<Integer> b) {
-        int match = 0;
         int count = 0;
-        if (a.size() == b.size()) {
-            for (Integer i : a) {
-                for (Integer j : b) {
-                    if ((i - j <= 2 && i - j > 0) || (j - i <= 2 && j - i > 0)) {
-                        count += 1;
 
-                    }
-                }
+        for (int i = 0; i < a.size(); i++) {
+            if (Math.abs(a.get(i) - b.get(i)) <= 2 && a.get(i) != b.get(i)) {
+                count++;
             }
         }
-        if (count == 4) {
-            match = 2;
-        }
-        if (count == 5) {
-            match = 3;
-        }
-        if (count == NUMBER) {
-            match = 2;
-        }
-        return match;
+        return count;
     }
 
     /**
@@ -51,16 +36,13 @@ public class Exam {
      * repeatSeparator("This", "And", 1) => "This"
      */
     public static String repeatSeparator(String word, String sep, int count) {
-        StringBuilder newString = new StringBuilder();
-        String onePart = word + sep;
         if (count == 1) {
             return word;
         }
         if (count > 1) {
             for (int i = 0; i < count - 1; i++) {
-                newString.append(onePart);
+                word = word + sep + word;
             }
-            return newString + word;
         }
         return word;
     }
