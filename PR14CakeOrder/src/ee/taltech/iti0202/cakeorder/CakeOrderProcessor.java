9a8a59e9079432f65ca323a5b50cc149fcddec26
jelen <jekibl@ttu.ee>
2019-05-08 19:09:59 +0300

    check style

--- PR14CakeOrder/src/ee/taltech/iti0202/cakeorder/CakeOrderProcessor.java
+++ PR14CakeOrder/src/ee/taltech/iti0202/cakeorder/CakeOrderProcessor.java
@@ -50,25 +50,25 @@ public class CakeOrderProcessor {
     public static void main(String[] args) {
         CakeOrderProcessor orderProcessor = new CakeOrderProcessor(CakeOrderProcessorType.MAKE_DAIRY_FREE);
         System.out.println(orderProcessor.process(
-                "{\n" +
-                        "  \"cakes\": [\n" +
-                        "    {\n" +
-                        "      \"name\": \"Sacher\",\n" +
-                        "      \"BBD\": \"2019-04-29\",\n" +
-                        "      \"price\": 14.00,\n" +
-                        "      \"kg\": 2.00,\n" +
-                        "      \"ingredients\": [\"flour\", \"chocolate\", \"milk\", \"sugar\", \"eggs\"]\n" +
-                        "    },\n" +
-                        "    {\n" +
-                        "      \"name\": \"New York Cheesecake\",\n" +
-                        "      \"BBD\": \"2019-04-30\",\n" +
-                        "      \"price\": 10.00,\n" +
-                        "      \"kg\": 1.50,\n" +
-                        "      \"ingredients\": [\"flour\", \"cream-cheese\", \"milk\", \"sugar\", \"eggs\"]\n" +
-                        "\n" +
-                        "    }\n" +
-                        "  ]\n" +
-                        "}"
+                "{\n"
+                        + "  \"cakes\": [\n"
+                        + "    {\n"
+                        + "      \"name\": \"Sacher\",\n"
+                        + "      \"BBD\": \"2019-04-29\",\n"
+                        + "      \"price\": 14.00,\n"
+                        + "      \"kg\": 2.00,\n"
+                        + "      \"ingredients\": [\"flour\", \"chocolate\", \"milk\", \"sugar\", \"eggs\"]\n"
+                        + "    },\n"
+                        + "    {\n"
+                        + "      \"name\": \"New York Cheesecake\",\n"
+                        + "      \"BBD\": \"2019-04-30\",\n"
+                        + "      \"price\": 10.00,\n"
+                        + "      \"kg\": 1.50,\n"
+                        + "      \"ingredients\": [\"flour\", \"cream-cheese\", \"milk\", \"sugar\", \"eggs\"]\n"
+                        + "\n"
+                        + "    }\n"
+                        + "  ]\n"
+                        + "}"
         ));
     }
 }
